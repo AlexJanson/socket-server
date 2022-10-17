09ddf2bd77beb71844e24e3f18688b4d07a1672c
= <=>
2022-10-17 21:57:27 +0200

    added cors preflight

--- api/channels-event.js
+++ api/channels-event.js
@@ -7,6 +7,21 @@ const {
     CLUSTER: cluster,
 } = process.env
 
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    return await fn(req, res)
+}
+
 const channels = new Channels({
     appId,
     key,
@@ -17,5 +32,7 @@ const channels = new Channels({
 export default async (req, res) => {
     const data = req.body
     await channels.trigger('event-channel', 'event-name', data)
-    return res.status(200).json('sent event successfully')
+    return res.status(200).end('sent event successfully')
 }
+
+module.exports = allowCors(handler)
